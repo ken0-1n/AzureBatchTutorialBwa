135edecaf36178e99cc446764bb2b4be98845344
Ubuntu <g13@chiba-ikaken.52ck1ssp2hiuphvor2z4m1tfhc.lx.internal.cloudapp.net>
2018-01-18 03:31:21 +0000

    create sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,27 @@
 import os
 import sys
 
-hogehoge = os.environ.get('hogehoge')
-print(hogehoge)
+import azure.storage.blob as azureblob
 
-testtest = sys.argv[1]
-print(testtest)
+if __name__ == '__main__':
+
+    hogehoge = sys.argv[1]
+    san = sys.argv[2]
+    sat = sys.argv[3]
+    testlog = open("test.log", 'w')
+    testlog.write(hogehoge)
+    testlog.close()
+
+    container = "output"
+    testlog_path = os.path.realpath("test.log")
+
+    print('Uploading file {} to container [{}]...'.format(
+        testlog_path,
+        container))
+
+    blob_client = azureblob.BlockBlobService(account_name=san,
+                                             sas_token=sat)
+    blob_client.create_blob_from_path(container,
+                                      "test.log",
+                                      testlog_path)
+
