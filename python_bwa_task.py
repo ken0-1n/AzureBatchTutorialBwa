534bc5e8fe47249ff2c43ee3a4f955a895ab1819
Ubuntu <g13@chiba-ikaken.52ck1ssp2hiuphvor2z4m1tfhc.lx.internal.cloudapp.net>
2018-01-23 06:13:17 +0000

    modify python_bwa_task.py

--- python_bwa_task.py
+++ python_bwa_task.py
@@ -48,9 +48,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     bwa = os.path.realpath(args.bwapath)
-    fastq1 = os.path.realpath(args.fastq1)
-    fastq2 = os.path.realpath(args.fastq2)
-    ref_fa = os.path.realpath(args.refgenome)
+    fastq1 = os.path.realpath(args.fastq1,starts, start='/mnt')
+    fastq2 = os.path.realpath(args.fastq2, start='/mnt')
+    ref_fa = os.path.realpath(args.refgenome, start='/mnt')
     samplename = args.samplename
 
     output_sam = samplename +'.sam'
